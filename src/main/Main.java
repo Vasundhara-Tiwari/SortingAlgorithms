34490d979a435886910c168542fb68baa9b13df4
Vasundhara Tiwari <vasundhara.tiwari_cs19@gla.ac.in>
2021-06-18 22:44:42 +0530

    Taking input of an array

--- src/main/Main.java
+++ src/main/Main.java
@@ -5,5 +5,13 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter the size of array :");
+        int size = scanner.nextInt();
+        int[] arr = new int[size];
+        System.out.println("Enter array elements: ");
+        for(int i=0;i<size; i++){
+            arr[i] = scanner.nextInt();
+        }
     }
 }
