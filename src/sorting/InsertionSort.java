0c53de9df23d0028b03c1353ff8573dafd5ad90c
Vasundhara Tiwari <vasundhara.tiwari_cs19@gla.ac.in>
2021-06-18 22:47:54 +0530

    Implementing insertion sorting algorithm

--- src/sorting/InsertionSort.java
+++ src/sorting/InsertionSort.java
@@ -3,7 +3,15 @@ package sorting;
 public class InsertionSort {
     public int[] sort(int[] arr){
 
-
+        for (int i = 1; i < arr.length; i++){
+            for (int j = 0; j < i; j++) {
+                if (arr[j] > arr[i]) {
+                    int temp = arr[j];
+                    arr[j] = arr[i];
+                    arr[i] = temp;
+                }
+            }
+        }
         return arr;
     }
 }
