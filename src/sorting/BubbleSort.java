cf01f3623a41760492093c2f7245d584dbc12737
Vasundhara Tiwari <vasundhara.tiwari_cs19@gla.ac.in>
2021-06-19 21:47:07 +0530

    Implementing Bubble Sort algorithm

--- src/sorting/BubbleSort.java
+++ src/sorting/BubbleSort.java
@@ -3,7 +3,9 @@ package sorting;
 public class BubbleSort {
     public int[] sort(int[] arr){
         for (int i=0;i< arr.length-1;i++){
+            for (int j = 0; j <arr.length-i; j++){
 
+            }
         }
         return arr;
     }
