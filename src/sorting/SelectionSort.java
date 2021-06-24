8e18d2fd22165134943a27e712ef5c60522f5843
Vasundhara Tiwari <vasundhara.tiwari_cs19@gla.ac.in>
2021-06-24 23:17:35 +0530

    Removing extra indents

--- src/sorting/SelectionSort.java
+++ src/sorting/SelectionSort.java
@@ -2,16 +2,15 @@ package sorting;
 
 public class SelectionSort {
     public int[] sort(int[] arr){
-
-            for (int i = 1; i < arr.length; i++) {
-                for (int j = 0; j < i; j++) {
-                    if (arr[j] > arr[i]) {
-                        int temp = arr[j];
-                        arr[j] = arr[i];
-                        arr[i] = temp;
-                    }
+        for (int i = 1; i < arr.length; i++) {
+            for (int j = 0; j < i; j++) {
+                if (arr[j] > arr[i]) {
+                    int temp = arr[j];
+                    arr[j] = arr[i];
+                    arr[i] = temp;
                 }
             }
-            return arr;
+        }
+        return arr;
     }
 }
