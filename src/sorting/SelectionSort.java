347a0ee61165f39dc7717cee0346671227de57b8
Vasundhara Tiwari <vasundhara.tiwari_cs19@gla.ac.in>
2021-06-24 23:16:28 +0530

    implementing selection sort algorithm

--- src/sorting/SelectionSort.java
+++ src/sorting/SelectionSort.java
@@ -3,8 +3,9 @@ package sorting;
 public class SelectionSort {
     public int[] sort(int[] arr){
 
+            for (int i = 1; i < arr.length; i++) {
 
-
-            return arr;
+            }
+                return arr;
     }
 }
