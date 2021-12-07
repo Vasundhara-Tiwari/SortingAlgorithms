faa29f6457777d2f67942e36c04bb40d0affeb35
Vasundhara-Tiwari <vasundhara.tiwari_cs19@gla.ac.in>
2021-12-07 19:53:47 +0530

    Insertion sort implementation in java

--- src/sorting/InsertionSort.java
+++ src/sorting/InsertionSort.java
@@ -1,5 +1,8 @@
 package sorting;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class InsertionSort {
     public int[] sort(int[] arr){
 
@@ -13,5 +16,19 @@ public class InsertionSort {
             }
         }
         return arr;
+    }
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int[] arr = new int[5];
+        for(int i=0; i< arr.length; i++){
+            arr[i] = scanner.nextInt();
+        }
+        System.out.println("Given Array");
+        System.out.println(Arrays.toString(arr));
+
+        InsertionSort is = new InsertionSort();
+
+        System.out.println("\nSorted array");
+        System.out.println(Arrays.toString(is.sort(arr)));
     }
 }
