10bae243bc2227003dc06c8d4b297b3d2f0d1aa3
Vasundhara-Tiwari <vasundhara.tiwari_cs19@gla.ac.in>
2021-12-08 23:21:56 +0530

    Counting sort implementation in java

---
+++ src/sorting/CountingSort.java
@@ -0,0 +1,41 @@
+package sorting;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class CountingSort {
+    void sort(int arr[])
+    {
+        int n = arr.length;
+        int output[] = new int[n];
+        int count[] = new int[256];
+        for (int i = 0; i < 256; ++i)
+            count[i] = 0;
+        for (int i = 0; i < n; ++i)
+            ++count[arr[i]];
+        for (int i = 1; i <= 255; ++i)
+            count[i] += count[i - 1];
+        for (int i = n - 1; i >= 0; i--) {
+            output[count[arr[i]] - 1] = arr[i];
+            --count[arr[i]];
+        }
+        for (int i = 0; i < n; ++i)
+            arr[i] = output[i];
+    }
+    public static void main(String args[])
+    {
+        Scanner scanner = new Scanner(System.in);
+        int[] arr = new int[5];
+        for(int i=0; i< arr.length; i++){
+            arr[i] = scanner.nextInt();
+        }
+        System.out.println("Given Array");
+        System.out.println(Arrays.toString(arr));
+
+        CountingSort cs = new CountingSort();
+        cs.sort(arr);
+
+        System.out.print("Sorted array is ");
+        System.out.println(Arrays.toString(arr));
+    }
+}
