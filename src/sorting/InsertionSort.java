66ee7833a6fdbe29c9cfb87d4d7643def665a24f
Vasundhara Tiwari <vasundhara.tiwari_cs19@gla.ac.in>
2022-07-27 23:15:09 +0530

    debugging

--- src/sorting/InsertionSort.java
+++ src/sorting/InsertionSort.java
@@ -5,16 +5,15 @@ import java.util.Scanner;
 
 public class InsertionSort {
     public int[] sort(int[] arr){
-
-        for (int i = 1; i < arr.length; i++){
-            for (int j = 0; j < i; j++) {
-                if (arr[j] > arr[i]) {
-                    int temp = arr[j];
-                    arr[j] = arr[i];
-                    arr[i] = temp;
-                }
+        int n = arr.length;
+        for (int i = 1; i < n; ++i) {
+            int key = arr[i];
+            int j = i - 1;
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j = j - 1;
             }
-        }
+            arr[j + 1] = key;
         return arr;
     }
     public static void main(String[] args) {
