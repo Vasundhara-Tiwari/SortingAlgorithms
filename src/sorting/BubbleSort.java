080080737e52cdc85611a8bdf62417cbb5c5cdf8
Vasundhara Tiwari <vasundhara.tiwari_cs19@gla.ac.in>
2022-07-27 23:10:52 +0530

    debugging

--- src/sorting/BubbleSort.java
+++ src/sorting/BubbleSort.java
@@ -7,9 +7,11 @@ public class BubbleSort {
     public int[] sort(int[] arr){
         for (int i=0;i< arr.length-1;i++){
             for (int j = 0; j <arr.length-i-1; j++){
-                int temp = arr[j];
-                arr[j] = arr[j +1];
-                arr[j +1]=temp;
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j +1];
+                    arr[j +1]=temp;
+                }
             }
         }
         return arr;
